ee1f2ccfaf88c0ba94ae5bf4a026f810849fcbb2
Christian Posta <christian.posta@gmail.com>
2014-05-15 20:10:58 -0400

    camel reverse proxy working

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Note, to use the fabric8-maven-plugin, you'll need this in your ~/.m2/settings.xml:
+
+<server>
+  <id>fabric8.upload.repo</id>
+  <username>admin</username>
+  <password>admin</password>
+</server>
+
+
+See more here: http://fabric8.io/#/site/book/doc/index.md?chapter=mavenPlugin_md
---
+++ http-gateway-proxy/src/main/java/com/redhat/httpgateway/Router.java
@@ -0,0 +1,29 @@
+package com.redhat.httpgateway;
+
+import org.apache.camel.Exchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ceposta on 5/15/14.
+ */
+public class Router {
+
+
+    private static final String BASE_FABRIC_NAME = "fabric:gateway";
+    private static final Logger LOG = LoggerFactory.getLogger(Router.class);
+
+    public String routePlease(Exchange exchange) {
+
+        // delegate to method that will do name/mapping resolution
+        return resolveEndpointName(exchange);
+    }
+
+    private String resolveEndpointName(Exchange exchange) {
+        // REAL CODE FOR MAPPING CAN GO HERE
+        String path = exchange.getIn().getHeader(Exchange.HTTP_URI, String.class);
+        String endpoint = BASE_FABRIC_NAME + path.replace("/", "");
+        LOG.info("Endpoint that the recipient list will use: {}", endpoint);
+        return endpoint;
+    }
+}
