4176f421bb2aaf8168f55a0ccb1a5831c3367473
michaelcostello <mcostell@redhat.com>
2014-05-21 17:25:53 -0400

    checking in for scrappy

--- http-gateway-proxy/src/main/java/com/redhat/httpgateway/Router.java
+++ http-gateway-proxy/src/main/java/com/redhat/httpgateway/Router.java
@@ -1,5 +1,9 @@
 package com.redhat.httpgateway;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +18,21 @@ public class Router {
     private static final String BASE_CXF_CLUSTER = "fabric:gateway";
     private static final Logger LOG = LoggerFactory.getLogger(Router.class);
     
+    private Map<String, String> someMap = new HashMap<String,String>(); 
+    private File someFile = null; 
+    private String endpointLocation; 
+    
+    public void init(){
+    	//load map from some properties file
+    	
+    }
     
 
     public String routePlease(Exchange exchange) {
 
         // delegate to method that will do name/mapping resolution
-        return resolveEndpointName(exchange);
+    	return someMap.get(exchange.getIn().getHeader(Exchange.HTTP_URI)); 
+       // return resolveEndpointName(exchange);
     }
     
     public String routeGateway(Exchange exchange){
